a33846ab422a7cff772f0597d582104c9a4c542b
vicfriedman <victoria.m.friedman@gmail.com>
2015-09-01 09:08:51 -0400

    done

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}" 
+  end
+
+  def list_roles
+    characters = Character.where("actor_id == id")
+    shows = []
+    characters.each do |char|
+      shows << Show.find_by_id(char.id)
+    end
+    sentence = ""
+    characters.each do |info|
+      shows.each do |show|
+       sentence += "#{info.name} - #{show.name}"
+      end 
+    end
+    sentence
+  end
   
 end
--- spec/actor_spec.rb
+++ spec/actor_spec.rb
@@ -1,4 +1,5 @@
 require_relative 'spec_helper'
+require 'pry'
 
 describe "Actor" do
   let(:actor) {Actor.new}
@@ -18,12 +19,14 @@ describe "Actor" do
   it "has associated characters in an array" do
     # Hint: think about what migration you'll need to write so that an actor can have many characters.
     # Where will the association foreign key go?
+    # binding.pry
     emilia = Actor.new(:first_name => "Emilia", :last_name => "Clarke")
     khaleesi = Character.new(:name => "Khaleesi")
     khaleesi.actor = emilia
     khaleesi.save
-    
+   
     khaleesi.reload
+    
     expect(emilia.characters).to include(khaleesi)
     expect(khaleesi.actor).to eq(emilia)
   end
